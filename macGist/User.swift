d81a42b882bbb292c985b079aac164273b184e68
Fernando Bunn <bunn@idevzilla.com>
2017-11-21 20:11:38 +0000

    Add user model

---
+++ macGist/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  macGist
+//
+//  Created by Fernando Bunn on 15/11/2017.
+//  Copyright © 2017 Fernando Bunn. All rights reserved.
+//
+
+import Foundation
+
+class User: Codable {
+    let login: String
+    let name: String
+}
